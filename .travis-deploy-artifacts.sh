e0281ce813056c828b0e623f072526150d6a14a2
Marcel Overdijk <marcel@overdijk.me>
2016-03-14 16:40:16 +0100

    Travis deploy doc artifacts

--- .travis-deploy-artifacts.sh
+++ .travis-deploy-artifacts.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+set +x
 
 # Do not deploy archives when building pull request
 if [ "$TRAVIS_BRANCH" != "master" ] || [ "$TRAVIS_PULL_REQUEST" == "true" ]; then
@@ -13,29 +14,20 @@ fi
 
 # Deploy api and reference documentation to gh-pages
 
-echo "Deploying docs"
-
 HAWAII_FRAMEWORK_VERSION=`cat gradle.properties | grep "version" | cut -d'=' -f2`
 GH_PAGES_DIR=.gh-pages
 
-echo "Deleting old gh-pages folder + cloning fresh repo"
-
 rm -rf $GH_PAGES_DIR
-git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/hawaiifw/hawaii-framework $GH_PAGES_DIR
-
-echo "Copying files"
+# git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/hawaiifw/hawaii-framework $GH_PAGES_DIR > /dev/null
+git clone --quiet --branch=gh-pages https://github.com/hawaiifw/hawaii-framework $GH_PAGES_DIR > /dev/null
 
 rm -rf $GH_PAGES_DIR/docs/$HAWAII_FRAMEWORK_VERSION
 mkdir -p $GH_PAGES_DIR/docs/$HAWAII_FRAMEWORK_VERSION
 unzip -o build/distributions/hawaii-framework-${HAWAII_FRAMEWORK_VERSION}-docs.zip -d $GH_PAGES_DIR/docs/$HAWAII_FRAMEWORK_VERSION
 
-echo "Pushing to gh-pages"
-
 git -C $GH_PAGES_DIR config user.email "travis@travis-ci.org"
 git -C $GH_PAGES_DIR config user.name "Travis"
 git -C $GH_PAGES_DIR commit --allow-empty -am "Travis build $TRAVIS_BUILD_NUMBER pushed docs to gh-pages"
-git -C $GH_PAGES_DIR push origin gh-pages
-
-echo "Pushed docs changed"
+git -C $GH_PAGES_DIR push origin gh-pages > /dev/null
 
 rm -rf $GH_PAGES_DIR
