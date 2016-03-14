3b5d36b1e25465c0932394caa25fbdf5003416da
Marcel Overdijk <marcel@overdijk.me>
2016-03-14 14:48:25 +0100

    Travis deploy doc artifacts

--- .travis-deploy-artifacts.sh
+++ .travis-deploy-artifacts.sh
@@ -17,7 +17,7 @@ HAWAII_FRAMEWORK_VERSION=`cat gradle.properties | grep "version" | cut -d'=' -f2
 GH_PAGES_DIR=.gh-pages
 
 rm -rf ${GH_PAGES_DIR}
-git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/hawaiifw/hawaii-framework ${GH_PAGES_DIR} > /dev/null
+git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/hawaiifw/hawaii-framework ${GH_PAGES_DIR}
 
 rm -rf ${GH_PAGES_DIR}/docs/${HAWAII_FRAMEWORK_VERSION}
 mkdir -p ${GH_PAGES_DIR}/docs/${HAWAII_FRAMEWORK_VERSION}
@@ -26,6 +26,6 @@ unzip -o build/distributions/hawaii-framework-${HAWAII_FRAMEWORK_VERSION}-docs.z
 git -C ${GH_PAGES_DIR} config user.email "travis@travis-ci.org"
 git -C ${GH_PAGES_DIR} config user.name "Travis"
 git -C ${GH_PAGES_DIR} commit --allow-empty -am "Travis build $TRAVIS_BUILD_NUMBER pushed docs to gh-pages"
-git -C ${GH_PAGES_DIR} push origin gh-pages > /dev/null
+git -C ${GH_PAGES_DIR} push origin gh-pages
 
 rm -rf ${GH_PAGES_DIR}
