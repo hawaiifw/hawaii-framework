15c470f520c28b9c0076eb9219e2826f73148324
Marcel Overdijk <marcel@overdijk.me>
2016-03-26 10:57:29 +0100

    Added alterative constructors

--- hawaii-core/src/main/java/org/hawaiiframework/validation/ValidationException.java
+++ hawaii-core/src/main/java/org/hawaiiframework/validation/ValidationException.java
@@ -19,6 +19,8 @@ package org.hawaiiframework.validation;
 import org.hawaiiframework.exception.HawaiiException;
 import org.springframework.util.Assert;
 
+import java.util.List;
+
 /**
  * @author Marcel Overdijk
  * @see ValidationResult
@@ -29,6 +31,13 @@ public class ValidationException extends HawaiiException {
     private final ValidationResult validationResult;
 
     /**
+     * Constructs a new {@link ValidationException} with an empty {@link ValidationResult}.
+     */
+    public ValidationException() {
+        this.validationResult = new ValidationResult();
+    }
+
+    /**
      * Constructs a new {@link ValidationException} with the supplied {@link ValidationResult}.
      *
      * @param validationResult the validation result
@@ -36,6 +45,28 @@ public class ValidationException extends HawaiiException {
     public ValidationException(ValidationResult validationResult) {
         Assert.notNull(validationResult, "Validation result must not be null");
         this.validationResult = validationResult;
+    }
+
+    /**
+     * Constructs a new {@link ValidationException} with the supplied {@link ValidationError}.
+     *
+     * @param validationError the validation error
+     */
+    public ValidationException(ValidationError validationError) {
+        this();
+        Assert.notNull(validationError, "Validation error must not be null");
+        validationResult.addError(validationError);
+    }
+
+    /**
+     * Constructs a new {@link ValidationException} with the supplied {@link ValidationError}s.
+     *
+     * @param validationErrors the validation errors
+     */
+    public ValidationException(List<ValidationError> validationErrors) {
+        this();
+        Assert.notNull(validationErrors, "Validation errors must not be null");
+        validationResult.addAllErrors(validationErrors);
     }
 
     /**
