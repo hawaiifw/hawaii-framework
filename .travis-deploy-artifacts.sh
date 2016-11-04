64d2df1b44ec1ab61fd42f62c54831a57b8df41b
Marcel Overdijk <marcel@overdijk.me>
2016-11-04 16:31:17 +0100

    Added signing of pom's for Maven release

--- .travis-deploy-artifacts.sh
+++ .travis-deploy-artifacts.sh
@@ -13,7 +13,7 @@ echo "Publishing archives..."
 
 openssl aes-256-cbc -pass pass:$SIGNING_PASSWORD -in secring.gpg.enc -out secring.gpg -d
 
-gpg --keyserver keyserver.ubuntu.com --recv-key $SIGNING_KEY
+gpg --keyserver hkp://pgp.mit.edu --recv-key $SIGNING_KEY
 
 ./gradlew -Psigning.keyId="$SIGNING_KEY" -Psigning.password="$SIGNING_PASSWORD" -Psigning.secretKeyRingFile="${TRAVIS_BUILD_DIR}/secring.gpg" uploadArchives
 
