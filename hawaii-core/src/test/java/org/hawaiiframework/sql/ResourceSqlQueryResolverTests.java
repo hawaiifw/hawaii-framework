6a117ee12101f910de794379c1206cccc80a0093
Marcel Overdijk <marcel@overdijk.me>
2017-04-21 14:49:30 +0200

    Polish

--- hawaii-core/src/test/java/org/hawaiiframework/sql/ResourceSqlQueryResolverTests.java
+++ hawaii-core/src/test/java/org/hawaiiframework/sql/ResourceSqlQueryResolverTests.java
@@ -9,9 +9,15 @@ import org.springframework.core.io.FileSystemResourceLoader;
 
 import java.io.File;
 import java.util.Arrays;
-import java.util.concurrent.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertThat;
 
