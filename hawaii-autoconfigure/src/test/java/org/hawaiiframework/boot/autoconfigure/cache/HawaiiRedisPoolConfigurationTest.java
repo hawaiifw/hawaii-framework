ee441f133c30df48e8f7a60bee7c9bce2e232aa7
marc.boulanger <hellhand@gmail.com>
2018-05-15 12:23:18 +0200

    Add functionality into Hawaii Framework.

--- hawaii-autoconfigure/src/test/java/org/hawaiiframework/boot/autoconfigure/cache/HawaiiRedisPoolConfigurationTest.java
+++ hawaii-autoconfigure/src/test/java/org/hawaiiframework/boot/autoconfigure/cache/HawaiiRedisPoolConfigurationTest.java
@@ -5,29 +5,27 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import redis.clients.jedis.JedisPoolConfig;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = {HawaiiRedisAutoConfiguration.class})
 public class HawaiiRedisPoolConfigurationTest {
+
+    @MockBean
+    JedisConnectionFactory jedisConnectionFactory;
 
     @Autowired
     private JedisPoolConfig jedisPoolConfig;
 
-    @Autowired
-    private JedisConnectionFactory jedisConnectionFactory;
-
     @Test
     public void assertRedisWiring() {
         assertNotNull(jedisPoolConfig);
-
         assertEquals(jedisPoolConfig.getMinEvictableIdleTimeMillis(), 30000);
-        assertNotNull(jedisConnectionFactory);
-        assertNotNull(jedisConnectionFactory.getHostName());
     }
 }
