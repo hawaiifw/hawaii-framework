207a0c23b230df6b6b250e9813bbbf034c308423
Rutger Lubbers <rutger.lubbers@qnh.nl>
2018-12-06 12:31:52 +0100

    Use delegating executor for the default executor.

--- hawaii-async/src/main/java/org/hawaiiframework/async/config/AsyncExecutorInitializer.java
+++ hawaii-async/src/main/java/org/hawaiiframework/async/config/AsyncExecutorInitializer.java
@@ -19,6 +19,7 @@ package org.hawaiiframework.async.config;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.concurrent.BasicThreadFactory;
 import org.hawaiiframework.async.AbortableTaskDecorator;
+import org.hawaiiframework.async.DelegatingExecutor;
 import org.hawaiiframework.async.model.ExecutorConfigurationProperties;
 import org.hawaiiframework.async.model.ExecutorProperties;
 import org.slf4j.Logger;
