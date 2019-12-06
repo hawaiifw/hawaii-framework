ae64d387492c8fad5db99349fafe01fc83ca7739
Maikel Geuns <mgeuns@ilionx.com>
2019-12-06 08:19:20 +0100

    Attempt #5

--- .travis-deploy-artifacts.sh
+++ .travis-deploy-artifacts.sh
@@ -15,7 +15,7 @@ openssl aes-256-cbc -K $encrypted_30f4b87b093a_key -iv $encrypted_30f4b87b093a_i
 
 echo "Publishing archives..."
 
-./gradlew publishToNexus -Psigning.keyId="$SIGNING_KEY" -Psigning.password="$SIGNING_PASSWORD" -Psigning.secretKeyRingFile="${TRAVIS_BUILD_DIR}/secring.gpg"
+./gradlew publish -Psigning.keyId="$SIGNING_KEY" -Psigning.password="$SIGNING_PASSWORD" -Psigning.secretKeyRingFile="${TRAVIS_BUILD_DIR}/secring.gpg"
 
 # Deploy api and reference documentation to gh-pages
 
