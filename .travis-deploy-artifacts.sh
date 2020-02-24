e85970800b77164ed2b0dbdfaaffc24b5c6c523c
Travis <travis@travis-ci.org>
2020-02-24 10:04:27 +0100

    Only build on specific branches

--- .travis-deploy-artifacts.sh
+++ .travis-deploy-artifacts.sh
@@ -6,6 +6,7 @@ HAWAII_FRAMEWORK_VERSION=`cat gradle.properties | grep "version" | cut -d'=' -f2
 
 # Do not deploy archives when building pull request
 if [ "$TRAVIS_BRANCH" != "master" ] && [ "$TRAVIS_BRANCH" != "2.x" ] || [ "$TRAVIS_PULL_REQUEST" == "true" ] || [[ $HAWAII_FRAMEWORK_VERSION == *SNAPSHOT* ]]; then
+  echo "Do not publish to sonatype..."
   exit 0
 fi
 
